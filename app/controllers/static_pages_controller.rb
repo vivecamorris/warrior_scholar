451fb98074b20156d902247006dd2cf579ef52dc
Phil Esterman <phil.esterman@yale.edu>
2014-07-22 11:48:28 -0400

    authentication must for each page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
+    before_action :signed_in_user, only: [:video1, :video2, :video3,
+     :video4, :video5, :map]
+
+
   def home
   end
 
@@ -22,5 +26,20 @@ class StaticPagesController < ApplicationController
 
   def map
   end
+
+
+
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+            #store_location
+          redirect_to signin_url, notice: "Please sign in." unless signed_in?
+        end
+
+
+  end
+
 
 end
