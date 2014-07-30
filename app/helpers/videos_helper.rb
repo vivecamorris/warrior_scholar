2169efbfed4f7cfa83046c259925c920997f7fbd
Phil Esterman <phil.esterman@yale.edu>
2014-07-29 21:26:05 -0400

    videos as RESTFUL!

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,32 +1,3 @@
 module VideosHelper
 
-	def test
-		puts "the helper worked!"
-	end
-	
-
-
-	def prev_and_next_vids(video)
-
-		if video.id > 1
-			@prev_vid = Video.find(video.id - 1)
-		else
-			@prev_vid = nil
-		end
-
-		if video.id < 6
-			@next_vid = Video.find(video.id + 1)
-		else
-			@next_vid = nil
-		end
-
-
-	end
-
-
-
-
-
-
-
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,24 @@
 class Video < ActiveRecord::Base
+
+
+
+
+
+	def prev_and_next_vids(video)
+
+			if video.id > 1
+				@prev_vid = Video.find(video.id - 1)
+			else
+				@prev_vid = nil
+			end
+
+			if video.id < 6
+				@next_vid = Video.find(video.id + 1)
+			else
+				@next_vid = nil
+			end
+
+		end
+
+
 end
