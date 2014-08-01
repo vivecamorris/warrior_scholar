679afae4aa50300e3290d7a2c6a43424762ae196
vivecamorris <viveca.morris@yale.edu>
2014-08-01 10:46:40 -0400

    added video descriptions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140729224829) do
+ActiveRecord::Schema.define(version: 20140801140912) do
 
   create_table "users", force: true do |t|
     t.string   "access_code"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20140729224829) do
     t.string   "subttl"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "videodescription"
   end
 
 end
