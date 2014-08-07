4d36e83376c16eaff1dd3cd4798cef9c85e28da3
Phil Esterman <phil.esterman@yale.edu>
2014-08-07 16:19:09 -0400

    changed title helper update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Warrior-Scholar Project Horizon"
+    base_title = "Warrior-Scholar Leadership Corps"
     if page_title.empty?
       base_title
     else
