3b2ce6eac17f6eaad8e4364be0c6661e61108f41
Phil Esterman <phil.esterman@yale.edu>
2014-08-09 12:53:24 -0400

    changed compressor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,8 +26,8 @@ Rails.application.configure do
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :sass
+config.assets.css_compressor = :yui
+config.assets.js_compressor = :uglify
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
